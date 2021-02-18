270204ac0aff83b8d95dc558d6e2d47eb42da08f
Jeffrey Scott Martin - Sulo Labs <sulo.labs@gmail.com>
2021-02-18 10:27:11 -0500

    adding functions webhook stripe

--- functions/handle-subscription-change.js
+++ functions/handle-subscription-change.js
@@ -32,8 +32,13 @@ exports.handler = async ({ body, headers }, context) => {
     const { netlifyID } = result.data.getUserByStripeID;
 
     // take the first word of the plan name and use it as the role
-    const plan = subscription.items.data[0].plan.nickname;
-    const role = plan.split(' ')[0].toLowerCase();
+    "prod_Iy8WKV4AR555nT"
+    const plan = subscription.items.data[0].plan.product;
+    const role;
+
+    if(plan === "prod_Iy8WKV4AR555nT") {
+        role = pro
+    }
 
     // send a call to the Netlify Identity admin API to update the user role
     const { identity } = context.clientContext;
